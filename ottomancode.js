b017e77240e829e8d0e7e4bc65b70394e67eed24
Glitch (dust-holy-cafe) <none>
2020-11-02 08:10:24 +0000

    🌥👔 Checkpoint ./komutlar/yardım.js:16568942/6 ./ottomancode.js:16568942/36 ./ayarlar.json:16568942/71

--- ottomancode.js
+++ ottomancode.js
@@ -124,8 +124,5 @@ client.on('warn', e => {
 client.on('error', e => {
     console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
-//KOMUTLAR BURAYA
 
-
-//KOMUTLAR BİTİŞ
 client.login(ayarlar.token);
