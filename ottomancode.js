63a9460991fca3877e58c3fb58f5155204b61bc5
Glitch (dust-holy-cafe) <none>
2021-07-25 22:57:21 +0000

    🍍🎉 Checkpoint ./ottomancode.js:34198417/3914 ./komutlar/hgbbayarla.js:34198417/894 ./komutlar/hgbbsıfırla.js:34198417/1168

--- ottomancode.js
+++ ottomancode.js
@@ -176,9 +176,130 @@ client.on('message', async message => {
     }
 })
 
-///otorol
+///resimlihgbb
 
+client.on("guildMemberRemove", async member => {
+  //let resimkanal = JSON.parse(fs.readFileSync("./ayarlar/gç.json", "utf8"));
+  //const canvaskanal = member.guild.channels.cache.get(resimkanal[member.guild.id].resim);
+  
+  if (db.has(`gçkanal_${member.guild.id}`) === false) return;
+  var canvaskanal = member.guild.channels.cache.get(db.fetch(`gçkanal_${member.guild.id}`));
+  if (!canvaskanal) return;
 
+  const request = require("node-superfetch");
+  const Canvas = require("canvas"),
+    Image = Canvas.Image,
+    Font = Canvas.Font,
+    path = require("path");
+
+  var randomMsg = ["Sunucudan Ayrıldı."];
+  var randomMsg_integer =
+    randomMsg[Math.floor(Math.random() * randomMsg.length)];
+
+  let msj = await db.fetch(`cikisM_${member.guild.id}`);
+  if (!msj) msj = `{uye}, ${randomMsg_integer}`;
+
+  const canvas = Canvas.createCanvas(640, 360);
+  const ctx = canvas.getContext("2d");
+
+  const background = await Canvas.loadImage(
+    "https://i.hizliresim.com/Wrn1XW.jpg"
+  );
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = "#74037b";
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = `#D3D3D3`;
+  ctx.font = `37px "Warsaw"`;
+  ctx.textAlign = "center";
+  ctx.fillText(`${member.user.username}`, 300, 342);
+
+  let avatarURL = member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 });
+  const { body } = await request.get(avatarURL);
+  const avatar = await Canvas.loadImage(body);
+
+  ctx.beginPath();
+  ctx.lineWidth = 4;
+  ctx.fill();
+  ctx.lineWidth = 4;
+  ctx.arc(250 + 55, 55 + 55, 55, 0, 2 * Math.PI, false);
+  ctx.clip();
+  ctx.drawImage(avatar, 250, 55, 110, 110);
+
+  const attachment = new Discord.MessageAttachment(
+    canvas.toBuffer(),
+    "ro-BOT-güle-güle.png"
+  );
+
+    canvaskanal.send(attachment);
+    canvaskanal.send(
+      msj.replace("{uye}", member).replace("{sunucu}", member.guild.name)
+    );
+    if (member.user.bot)
+      return canvaskanal.send(`🤖 Bu bir bot, ${member.user.tag}`);
+  
+});
+
+client.on("guildMemberAdd", async member => {
+  if (db.has(`gçkanal_${member.guild.id}`) === false) return;
+  var canvaskanal = member.guild.channels.cache.get(db.fetch(`gçkanal_${member.guild.id}`));
+
+  if (!canvaskanal || canvaskanal ===  undefined) return;
+  const request = require("node-superfetch");
+  const Canvas = require("canvas"),
+    Image = Canvas.Image,
+    Font = Canvas.Font,
+    path = require("path");
+
+  var randomMsg = ["Sunucuya Katıldı."];
+  var randomMsg_integer =
+    randomMsg[Math.floor(Math.random() * randomMsg.length)];
+
+  let paket = await db.fetch(`pakets_${member.id}`);
+  let msj = await db.fetch(`cikisM_${member.guild.id}`);
+  if (!msj) msj = `{uye}, ${randomMsg_integer}`;
+
+  const canvas = Canvas.createCanvas(640, 360);
+  const ctx = canvas.getContext("2d");
+
+  const background = await Canvas.loadImage(
+    "https://i.hizliresim.com/UyVZ4f.jpg"
+  );
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = "#74037b";
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = `#D3D3D3`;
+  ctx.font = `37px "Warsaw"`;
+  ctx.textAlign = "center";
+  ctx.fillText(`${member.user.username}`, 300, 342);
+
+  let avatarURL = member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }) ;
+  const { body } = await request.get(avatarURL);
+  const avatar = await Canvas.loadImage(body);
+
+  ctx.beginPath();
+  ctx.lineWidth = 4;
+  ctx.fill();
+  ctx.lineWidth = 4;
+  ctx.arc(250 + 55, 55 + 55, 55, 0, 2 * Math.PI, false);
+  ctx.clip();
+  ctx.drawImage(avatar, 250, 55, 110, 110);
+
+  const attachment = new Discord.MessageAttachment(
+    canvas.toBuffer(),
+    "ro-BOT-hosgeldin.png"
+  );
+
+  canvaskanal.send(attachment);
+  canvaskanal.send(
+    msj.replace("{uye}", member).replace("{sunucu}", member.guild.name)
+  );
+  if (member.user.bot)
+    return canvaskanal.send(`🤖 Bu bir bot, ${member.user.tag}`);
+});
 
 
 
